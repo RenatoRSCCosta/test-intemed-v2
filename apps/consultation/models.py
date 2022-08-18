d5113a8a5a11805d89f5e95742d8e671cc5a3aa4
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-08-18 01:47:00 -0300

    feat: add consultation app

---
+++ apps/consultation/models.py
@@ -0,0 +1,11 @@
+from re import M
+from django.db import models
+from schedule.models import Schedule, Schedules
+
+class Consultation(models.Model):
+    schedule_id = models.ForeignKey(Schedule, related_name="Schedule", on_delete=models.CASCADE)
+    schedules_id = models.ForeignKey(Schedules, related_name="Schedules", on_delete=models.CASCADE)
+    appointment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Consulta no horario de {self.schedules_id.hour} no dia {self.schedule_id.date}"
