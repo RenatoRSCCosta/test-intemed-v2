9082c470de24bd3069a2c52ba59a60c15541e87d
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-08-18 19:14:44 -0300

    core: adds meta class in doctor models

--- apps/doctor/models.py
+++ apps/doctor/models.py
@@ -1,4 +1,3 @@
-from operator import mod
 from django.db import models
 
 class Specialty(models.Model):
@@ -11,7 +10,11 @@ class Doctor(models.Model):
     specialty_id = models.ForeignKey(Specialty, related_name="Specialty", on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     crm = models.CharField(max_length=10)
+    email = models.EmailField(max_length=255, blank=True, null=True)
     
     def __str__(self):
         return self.name
+    
+    class Meta:     
+        ordering = ["name"]
     
