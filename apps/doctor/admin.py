9f57e8835ccb34625dcd4855ed96d0799aedbae5
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-08-20 05:04:21 -0300

    core: admin doctor

--- apps/doctor/admin.py
+++ apps/doctor/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from doctor.models import Doctor, Specialty
 
-# Register your models here.
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ("id","name", "crm", "email",)
+    list_display_links = ("id","name",)
+
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = ("id","name",)
+    list_display_links = ("id","name",)
+    
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Specialty, SpecialtyAdmin)
+    
