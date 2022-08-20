0d3089a1d92b31229f3608129ebddab8ea398d8d
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-08-20 04:46:41 -0300

    fix: related_name and unique_together

--- apps/consultation/models.py
+++ apps/consultation/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from schedule.models import Schedule, Schedules
 
 class Consultation(models.Model):
-    schedule_id = models.ForeignKey(Schedule, related_name="Schedule", on_delete=models.CASCADE)
-    schedules_id = models.ForeignKey(Schedules, related_name="Schedules", on_delete=models.CASCADE)
+    schedule_id = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    schedules_id = models.ForeignKey(Schedules, on_delete=models.CASCADE)
     appointment_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Consulta no horario de {self.schedules_id.hour} no dia {self.schedule_id.date}"
     
     class Meta:
-        unique_together = ("shedule_id", "shedules_id")
+        unique_together = ("schedule_id", "schedules_id")
