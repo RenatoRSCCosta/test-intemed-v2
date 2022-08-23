21739928c36dae40884e0899e0f1b52545439946
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-08-23 03:28:43 -0300

    feat: inline model schedules

--- apps/schedule/admin.py
+++ apps/schedule/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from schedule.models import Schedule, Schedules
 
+class SchedulesInline(admin.TabularInline):
+    model = Schedules
+
 class ScheduleAdmin(admin.ModelAdmin):
-    list_display = ("id", "doctor_id", "date","availabe")
-    list_display_links = ("id","doctor_id")
-    
-class SchedulesAdmin(admin.ModelAdmin):
-    list_display = ("id", "hour","availabe")
-    list_display_links = ("id","hour")
+    list_display = ("id", "doctor", "date","availabe")
+    list_display_links = ("id","doctor")
+    inlines = [SchedulesInline]
     
 admin.site.register(Schedule, ScheduleAdmin)
-admin.site.register(Schedules, SchedulesAdmin)
