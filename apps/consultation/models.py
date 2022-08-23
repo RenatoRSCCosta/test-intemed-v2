4ece4be6d12ebd3947def4f2a39fc949e19a8903
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-08-23 02:52:31 -0300

    fix: rename foreign keys models

--- apps/consultation/models.py
+++ apps/consultation/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from schedule.models import Schedule, Schedules
 
 class Consultation(models.Model):
-    schedule_id = models.ForeignKey(Schedule, on_delete=models.CASCADE)
-    schedules_id = models.ForeignKey(Schedules, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    schedules = models.ForeignKey(Schedules, on_delete=models.CASCADE)
     appointment_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- apps/schedule/models.py
+++ apps/schedule/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from doctor.models import Doctor
 
 class Schedule(models.Model):
-    doctor_id = models.ForeignKey(Doctor, related_name="Doctor", on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, related_name="Doctor", on_delete=models.CASCADE)
     date = models.DateField()
     availabe = models.BooleanField(default=True)
 
@@ -13,9 +13,9 @@ class Schedule(models.Model):
         ordering = ["date"]
     
 class Schedules(models.Model):
-    schedule_id = models.ForeignKey(Schedule, related_name="Schedule", on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, related_name="Schedule", on_delete=models.CASCADE)
     hour = models.TimeField()
-    available = models.BooleanField(default=True)
+    availabe = models.BooleanField(default=True)
     
     def __str__(self):
         return f"horario {self.hour} para agenda na data de {self.schedule_id.date}"
