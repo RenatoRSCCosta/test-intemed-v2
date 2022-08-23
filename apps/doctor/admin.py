9f0bf23d05e9ec2ed2bea495914b51e06c248593
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-08-23 02:49:35 -0300

    feat: register doctor and specialty models admin

--- apps/doctor/admin.py
+++ apps/doctor/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from doctor.models import Doctor, Specialty
 
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ("id","name", "crm", "email",)
+    list_display = ("id","name", "crm", "email","specialty",)
     list_display_links = ("id","name",)
 
 class SpecialtyAdmin(admin.ModelAdmin):
