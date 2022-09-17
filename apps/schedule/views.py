55f2caf4599e4f722339da734f441b52c2a15df0
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-09-16 22:04:33 -0300

    feat: schedule view

--- apps/schedule/views.py
+++ apps/schedule/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from datetime import date, datetime
+from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from schedule.models import Schedule
+from schedule.serializer import ScheduleSerializer
 
-# Create your views here.
+
+class ScheduleViewSet(GenericViewSet):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+    
+    def get_queryset(self):
+        today = date.today()
+        now = datetime.now().strftime('%H:%M')
+        self.queryset = Schedule.objects.filter(Q(date__gt=today) | Q(date=today, Schedule__hour__gte=now)).distinct()
+        return self.queryset
+    def list(self, *args, **kwargs):
+        qs = self.get_queryset()
+        serializer = ScheduleSerializer(qs, many=True)
+        return Response(serializer.data)
