ac94e8a4f753d6efea35a7e9d76e540aee81d4c8
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-09-19 17:07:41 -0300

    feat: get_schedules

--- apps/schedule/serializer.py
+++ apps/schedule/serializer.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from schedule.models import Schedule, Schedules
+from schedule.validators import schedules_available
 from doctor.serializer import DoctorSerializer
         
 class SchedulesSerializer(serializers.ModelSerializer):
+    
+    def to_representation(self, instance):
+        return str(instance.hour)[:5]
+    
     class Meta:
         model = Schedules
         fields = (
@@ -10,13 +15,20 @@ class SchedulesSerializer(serializers.ModelSerializer):
         )
         
 class ScheduleSerializer(serializers.ModelSerializer):
-    Schedule = serializers.StringRelatedField(many=True)
+
+    schedules = serializers.SerializerMethodField('get_schedules')
     doctor = DoctorSerializer(many=False)
+    
+    def get_schedules(self, schedule):
+        qs = schedules_available(schedule.id)
+        serializer = SchedulesSerializer(instance=qs, many=True)
+        return serializer.data
+    
     class Meta:
         model = Schedule
         fields = (
             'id',
             'doctor', 
             'date',
-            'schedules_schedule' 
+            'schedules' 
         )
