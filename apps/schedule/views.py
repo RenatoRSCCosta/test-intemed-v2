9f931e22f3564ebb551db827cb1ba3420db49c0e
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-09-19 17:08:28 -0300

    fix: get_queryset

--- apps/schedule/views.py
+++ apps/schedule/views.py
@@ -1,20 +1,24 @@
 from datetime import date, datetime
 from django.db.models import Q
+#from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-from schedule.models import Schedule
-from schedule.serializer import ScheduleSerializer
+from schedule.models import Schedule, Schedules
+from schedule.serializer import ScheduleSerializer, SchedulesSerializer
+#from schedule.filters import ScheduleFilter
 
 
 class ScheduleViewSet(GenericViewSet):
     queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
+    #filter_backends = (DjangoFilterBackend,)
+    #filterset_fields = ('doctor',)
+    #filterset_class = ScheduleFilter
     
     def get_queryset(self):
         today = date.today()
         now = datetime.now().strftime('%H:%M')
-        self.queryset = Schedule.objects.filter(Q(date__gt=today) | Q(date=today, schedules_schedule__hour__gte=now)).distinct()
-        return self.queryset
+        return self.queryset.filter(date__gte=today, schedules_schedule__hour__gte=now)
     
     def list(self, *args, **kwargs):
         qs = self.get_queryset()
