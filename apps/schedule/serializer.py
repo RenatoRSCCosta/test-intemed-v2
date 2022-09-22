75ce05722fc19a9cb09ea2c78fa7e556e48f6fcb
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-09-21 23:02:37 -0300

    fix: get schedules

--- apps/schedule/serializer.py
+++ apps/schedule/serializer.py
@@ -1,28 +1,27 @@
+from asyncore import read
 from rest_framework import serializers
 from schedule.models import Schedule, Schedules
 from schedule.validators import schedules_available
 from doctor.serializer import DoctorSerializer
+from datetime import datetime, date
+from django.db.models import Q
         
 class SchedulesSerializer(serializers.ModelSerializer):
-    
-    def to_representation(self, instance):
-        return str(instance.hour)[:5]
     
     class Meta:
         model = Schedules
         fields = (
             'hour',
         )
+    
+    def to_representation(self, instance):
+        return str(instance.hour)[:5]
+    
         
 class ScheduleSerializer(serializers.ModelSerializer):
 
-    schedules = serializers.SerializerMethodField('get_schedules')
-    doctor = DoctorSerializer(many=False)
-    
-    def get_schedules(self, schedule):
-        qs = schedules_available(schedule.id)
-        serializer = SchedulesSerializer(instance=qs, many=True)
-        return serializer.data
+    schedules = serializers.SerializerMethodField('get_schedules', read_only=True)
+    doctor = DoctorSerializer(many=False, read_only=True)
     
     class Meta:
         model = Schedule
@@ -32,3 +31,10 @@ class ScheduleSerializer(serializers.ModelSerializer):
             'date',
             'schedules' 
         )
+        
+    def get_schedules(self, schedule_id):
+        qs = Schedules.objects.filter(Q (schedule=schedule_id, schedule__date=date.today(), 
+                                         hour__gte=datetime.now().strftime('%H:%M'), available=True) 
+                                     |Q (schedule=schedule_id, schedule__date__gte=date.today(), available=True))
+        serializer = SchedulesSerializer(instance=qs, many=True)
+        return serializer.data
