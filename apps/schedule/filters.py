a658fb2f8ce2e2ab981538d66790218a3760589b
Renato Costa <renatosilvacosta1998@gmail.com>
2022-09-25 22:06:33 -0300

    fix: data_inicio filter

--- apps/schedule/filters.py
+++ apps/schedule/filters.py
@@ -5,7 +5,7 @@ from schedule.models import Schedule
 class ScheduleFilter(filters.FilterSet):
     doctor = filters.NumberFilter(lookup_expr='exact')
     crm = filters.CharFilter(field_name='doctor__crm',lookup_expr='exact')
-    data_inicio = filters.DateFilter(field_name='date',lookup_expr='contains')
+    data_inicio = filters.DateFilter(field_name='date',lookup_expr='gte')
     data_final = filters.DateFilter(field_name='date',lookup_expr='lte')
     
     class Meta:
