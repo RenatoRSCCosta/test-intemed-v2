a76884b7f2bd1ce8a952bd25efc2b8761d8c9628
Renato Costa <45673000+RenatoRSCCosta@users.noreply.github.com>
2022-11-18 21:30:13 -0300

    feat: register url router

--- setup/urls.py
+++ setup/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
 from apps.schedule.views import ScheduleViewSet
+from apps.consultation.views import ConsultationViewSet
 
 router = routers.DefaultRouter()
 router.register('schedules', ScheduleViewSet, basename="schedule")
+router.register('consultations', ConsultationViewSet, basename="consultation")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
